f4bcdee6f780c01bfd9cacc3e379fd5791b5b170
Lyrisbee <xicadfta@gmail.com>
2017-06-04 16:27:25 +0800

    add button

---
+++ layout/main_button.php
@@ -0,0 +1,35 @@
+<style>
+    div.btn_surface{
+        width:70%;
+        margin-left: 14em;
+        margin-right: 14em;
+    }
+    body{
+      display: flex;
+      align-items:flex-end;
+    }
+    button{
+       width:10em;
+    }
+    .logout{
+      justify-content:flex-end;
+    }
+</style>
+<div class="btn_surface">
+  <div >
+      <div class="col-lg-2"><button class="btn btn-default" type="button" name="button">東</button></div>
+      <div class="col-lg-2"><button class="btn btn-default" type="button" name="button">西</button></div>
+      <div class="col-lg-8"><button class="btn btn-default" type="button" name="button">攻擊</button></div>
+  </div>
+  <div = "row">
+      <div class="col-md-2"><button class="btn btn-default" type="button" name="button">南</button></div>
+      <div class="col-md-2"><button class="btn btn-default" type="button" name="button">北</button></div>
+      <div class="col-md-2"><button class="btn btn-default" type="button" name="button">回城</button></div>
+      <div class="col-md-2 col-md-offset-4"><button class="btn btn-default" type="button" name="button">登出</button></div>
+  </div>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
