0b3b5b450d6db94c280b56d0b027a1f4cefa90ea
Lyrisbee <xicadfta@gmail.com>
2017-06-05 22:22:20 +0800

    fix bug #4

--- layout/main_button.php
+++ layout/main_button.php
@@ -18,7 +18,7 @@
       <div>
           <div class="col-lg-2"><button class="btn btn-default" type="submit" name="east" <?php if($map_data['toE'] == '0')echo "disabled"; ?>>東</button></div>
           <div class="col-lg-2"><button class="btn btn-default" type="submit" name="west" <?php if($map_data['toW'] == '0')echo "disabled"; ?>>西</button></div>
-          <div class="col-lg-8"><button class="btn btn-default" type="submit" name="battle" <?php if($map_data['class'] == 'npc')echo "disabled"; ?>>戰鬥</button></div>
+          <div class="col-lg-8"><button class="btn btn-default" type="submit" name="battle" <?php if($map_data['class'] != 'mon')echo "disabled"; ?>>戰鬥</button></div>
       </div>
       <div = "row">
           <div class="col-md-2"><button class="btn btn-default" type="submit" name="south" <?php if($map_data['toS'] == '0')echo "disabled"; ?>>南</button></div>
