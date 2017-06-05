109e4eaf8c73b87a845edd1fc0cae80892312f54
Lyrisbee <xicadfta@gmail.com>
2017-06-05 09:40:32 +0800

    battle

--- main.php
+++ main.php
@@ -1,3 +1,17 @@
+<?php
+session_start();
+ob_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include("db/db_config.php");
+if(isset($_SESSION['name'])){
+    include("php/map_load.php");
+}else{
+  header('Location: login.php');
+  exit;
+}
+//check login time set x,y
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -7,37 +21,14 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-    <link href="css/role.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+
     <title>Role</title>
   </head>
   <body>
-    <h1>職業選擇</h1>
-    <div class="role">
-
-      <form class="" action="role.php"  method="post">
-              <div class="form-group">
-                <label for="nickname">暱稱</label>
-                <input type="text" class="form-control" value="<?php if(isset($_POST['nickname'])){echo $_POST['nickname'];} ?>" id="nickname" name="nickname" placeholder="暱稱" required="required">
-              </div>
-              <div class="form-group">
-                <label for="role">性別</label>
-                <select class="form-control" id="role">
-                    <option>男</option>
-                    <option>女</option>
-                </select>
-              </div>
-              <div class="form-group">
-                  <label for="role">職業</label>
-                  <select class="form-control" id="role">
-                      <option>測試者</option>
-                  </select>
-              </div>
-            <div class="form-group sub">
-  						<button type="submit" name="checkbtn" id="checkbtn">確定</button>
-  					</div>
-      </form>
-    </div>
-
+      <p><?php print_r( $map_data );?></p>
+      <p><?php print_r( $role_data );?></p>
+      <?php include('layout/main_button.php'); ?>
   </body>
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
